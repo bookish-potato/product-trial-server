2d1cbf8c1990a856a69e7f212d817d977afee65b
Fengyi Li <68875335+Fengyi-Li@users.noreply.github.com>
2022-05-12 20:47:07 +0000

    add new APIs to the simple server

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
 const port = 3000;
+const names = [];
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
   console.log(`${new Date()} ${req.method} ${req.path}`);
   res.send("Hello World!");
+});
+
+app.get("/time", (req, res) => {
+  console.log(`${new Date()} ${req.method} ${req.path}`);
+  res.send(new Date().toString());
+});
+
+app.get("/names/:name", (req, res) => {
+  console.log(`${new Date()} ${req.method} ${req.path}`);
+  var nameInput = req.params.name;
+  if (names.includes(nameInput))
+  {
+    res.send(`Hello! Your name is ${nameInput}. `);
+  }
+  else 
+  {
+    res.send(`Sorry! Name not found!`)
+  }
+});
+
+/*  the request body contract
+{
+  "name": string
+}
+*/
+app.post("/names/add", (req, res) => {
+  console.log(`${new Date()} ${req.method} ${req.path}`);
+  const nameInput = req.body.name
+  names.push(nameInput)
+  res.send("A new name is added successfully!")
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
